e649b81134ae1dfd130953b72353a53c141b8927
William-L79 <33826890+William-L79@users.noreply.github.com>
2017-12-20 08:12:28 +0100

    Add files via upload

---
+++ back_office.blade.php
@@ -0,0 +1,61 @@
+@extends( 'layout' )
+@section('title')
+Accueil
+@endsection
+@section('content')
+<h1> Back-office </h1>
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#tuile">
+Voir tuiles
+</button>
+<div class="modal fade" id="tuile" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Liste tuiles</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        </button>
+      </div>
+      <div class="modal-body">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Titre</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" data-dismiss="modal">Retour</button>
+    </div>
+  </div>
+</div>
+
+
+
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#add">
+Ajouter
+</button>
+<div class="modal fade" id="add" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Ajouter une tuile</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        </button>
+      </div>
+      <div class="modal-body">
+
+      </div>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" data-dismiss="modal">Retour</button>
+      <button type="button" class="btn btn-primary  " data-dismiss="modal">Ajouter</button>
+    </div>
+  </div>
+</div>
+@endsection
