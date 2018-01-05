12602e0bd395f2d99feac4c5ad168e4dd4016d55
David Traparic <david.traparic@etu.univ-poitiers.fr>
2018-01-05 18:25:18 +0100

    remise en ordre des dossiers sans majuscule

--- app/Models/Admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Models;
-
-class Admin extends Model{
-  protected $allowed_properties = ['id', 'pseudo', 'password', 'email'];
-  protected $pk_name = 'id';
-}
-?>
--- app/Models/Tile.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Models;
-
-class Tile extends Model{
-  protected $allowed_properties = ['id', 'title', 'description', 'image', 'layout', 'bg_color_text', 'color_text', 'position', 'bg_color_image', 'auto_size'];
-  protected $pk_name = 'id';
-}
-?>
