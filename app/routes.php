aad6c81783bb62ebe06309a5c93a626a4ac468fa
David Traparic <david.traparic@etu.univ-poitiers.fr>
2018-01-11 18:13:38 +0100

    fix bologin, controller, maybe populate

--- app/routes.php
+++ app/routes.php
@@ -8,6 +8,7 @@ $route->addRoute( 'POST' , '/tile/add'                , 'TileController@addSave'
 $route->addRoute( 'GET'  , '/tile/edit/{id:[0-9]+}'   , 'TileController@edit' );
 $route->addRoute( 'POST' , '/tile/edit/{id:[0-9]+}'   , 'TileController@editSave' );
 
+$route->addRoute( 'GET' , '/bo'               , 'BoController@index' );
 $route->addRoute( 'GET' , '/backoffice'               , 'BoController@index' );
 $route->addRoute( 'GET' , '/login'                    , 'BoController@login' );
 $route->addRoute( 'POST', '/login'                    , 'BoController@checkLogin' );
--- app/views/backoffice/boindex.blade.php
+++ app/views/backoffice/boindex.blade.php
@@ -36,26 +36,44 @@ Voir tuiles
 </div>
 
 
-
 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#add">
 Ajouter
 </button>
 <div class="modal fade" id="add" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Ajouter une tuile</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-        </button>
-      </div>
-      <div class="modal-body">
+    <form action="">
+      <div class="modal-content">
 
+        <div class="modal-header">
+          <h5 class="modal-title">Ajouter une tuile</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">iCloseFont</button>
+        </div>
+
+        <div class="modal-body">
+
+          <div class="form-group">
+            <label for="exampleInputEmail1">Titre</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Entrer titre">
+          </div>
+
+          <div class="form-group">
+            <label for="exampleInputEmail1">Description</label>
+            <textarea type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Écrire description"></textarea>
+          </div>
+
+            <div class="custom-file col-1">
+              <input type="file" class="custom-file-input" id="customFile">
+              <label class="custom-file-label" for="customFile">Choose file</label>
+            </div>
+
+        </div>
       </div>
-    </div>
-    <div class="modal-footer">
-      <button type="button" class="btn btn-secondary" data-dismiss="modal">Retour</button>
-      <button type="button" class="btn btn-primary  " data-dismiss="modal">Ajouter</button>
-    </div>
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Retour</button>
+        <button type="button" class="btn btn-primary  " data-dismiss="modal">Ajouter</button>
+      </div>
+    </form>
   </div>
 </div>
 @endsection
